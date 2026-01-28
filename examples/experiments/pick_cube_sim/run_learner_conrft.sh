cc349f209920fa574f58e95c4ba8a0af4685bd5c
pgq18 <penggq18@163.com>
2026-01-28 22:50:16 +0800

    Update checkpoint paths to local workspace directory

--- examples/experiments/pick_cube_sim/run_learner_conrft.sh
+++ examples/experiments/pick_cube_sim/run_learner_conrft.sh
@@ -2,7 +2,7 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.5 && \
 python ../../train_conrft_octo.py "$@" \
     --exp_name=pick_cube_sim \
-    --checkpoint_path=/root/online_rl/conrft/examples/experiments/pick_cube_sim/checkpoint \
+    --checkpoint_path=/home/pgq/Workspace/VLA/conrft-modified/examples/experiments/pick_cube_sim/checkpoint \
     --q_weight=1.0 \
     --bc_weight=0.1 \
     --demo_path=./demo_data/pick_cube_sim_30_demos_2026-01-27_20-19-11.pkl \
