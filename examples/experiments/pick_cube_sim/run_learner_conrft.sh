75205a1744afb78154aec71b532813b97ecc84b1
pgq18 <penggq18@163.com>
2026-01-29 22:34:17 +0800

    test param

--- examples/experiments/pick_cube_sim/run_learner_conrft.sh
+++ examples/experiments/pick_cube_sim/run_learner_conrft.sh
@@ -3,8 +3,8 @@ export XLA_PYTHON_CLIENT_MEM_FRACTION=.9 && \
 python ../../train_conrft_octo.py "$@" \
     --exp_name=pick_cube_sim \
     --checkpoint_path=/home/pgq/Workspace/VLA/conrft-modified/examples/experiments/pick_cube_sim/checkpoint \
-    --q_weight=1.0 \
-    --bc_weight=0.5 \
+    --q_weight=0.1 \
+    --bc_weight=1.0 \
     --demo_path=./demo_data/pick_cube_sim_30_demos_2026-01-27_20-19-11.pkl \
     --pretrain_steps=20000 \
     --debug=False \
