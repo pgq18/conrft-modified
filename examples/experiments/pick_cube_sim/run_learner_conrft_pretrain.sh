b954842145d97381b5b21d3d952b923cec224fd2
pgq18 <penggq18@163.com>
2026-01-30 15:17:25 +0800

    new training test

--- examples/experiments/pick_cube_sim/run_learner_conrft_pretrain.sh
+++ examples/experiments/pick_cube_sim/run_learner_conrft_pretrain.sh
@@ -4,8 +4,8 @@ python ../../train_conrft_octo.py "$@" \
     --exp_name=pick_cube_sim \
     --q_weight=0.1 \
     --bc_weight=1.0 \
-    --demo_path=./demo_data/pick_cube_sim_30_demos_2026-01-27_20-19-11.pkl \
+    --demo_path=./demo_data/pick_cube_sim_30_demos_2026-01-30_15-11-16.pkl \
     --pretrain_steps=20000 \
     --debug=False \
     --learner \
-    --checkpoint_path=/home/pgq/Workspace/VLA/conrft-modified/examples/experiments/pick_cube_sim/checkpoint \
+    --checkpoint_path=/home/pgq/Workspace/VLA/conrft-modified/examples/experiments/pick_cube_sim/checkpoint_1 \
