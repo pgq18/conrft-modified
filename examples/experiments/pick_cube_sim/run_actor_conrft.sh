9edafc99adab95ad75fae197973ba64294101450
pgq18 <penggq18@163.com>
2026-02-02 16:35:45 +0800

    sync

--- examples/experiments/pick_cube_sim/run_actor_conrft.sh
+++ examples/experiments/pick_cube_sim/run_actor_conrft.sh
@@ -2,7 +2,7 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=true && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.8 && \
 python ../../train_conrft_octo.py "$@" \
     --exp_name=pick_cube_sim \
-    --checkpoint_path=/home/pgq/Workspace/VLA/conrft-modified/examples/experiments/pick_cube_sim/checkpoint_1 \
+    --checkpoint_path=/home/pgq/Workspace/VLA/conrft-modified/examples/experiments/pick_cube_sim/checkpoint_2 \
     --actor \
     --ip "10.20.85.133" \
     --show_camera
